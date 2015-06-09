152edef99398b0b0498735e97e6de905e5e54b16
Marina Ushakova <mushako1@my.westga.edu>
2015-06-09 03:36:50 -0700

    Connection string is changes to localhost. Added login action to MyAccountController.

--- GoFit/Controllers/MyAccountController.cs
+++ GoFit/Controllers/MyAccountController.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using GoFit.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace GoFit.Controllers
 {
@@ -11,6 +13,27 @@ namespace GoFit.Controllers
         
         public ActionResult Login()
         {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(Login login, string ReturnUrl = "")
+        {
+            if (login.Username.Equals("user") && login.Password.Equals("password"))
+            {
+                FormsAuthentication.SetAuthCookie("user", login.RememberMe);
+
+                if (Url.IsLocalUrl(ReturnUrl))
+                {
+                    return Redirect(ReturnUrl);
+                }
+                else
+                {
+                    return RedirectToAction("MyProfile", "Home");
+                }
+            }
+            ModelState.Remove("Password");
             return View();
         }
 	}
