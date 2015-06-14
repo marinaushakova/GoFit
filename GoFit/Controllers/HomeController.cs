884b6a75205625a146dc0c79e3dc45f2e52e73b0
Kevin Carlson <kcarlso2@my.westga.edu>
2015-06-14 16:33:03 -0400

    Implemented controller functionality to sort workouts by name, user, date added, and category

--- GoFit/Controllers/HomeController.cs
+++ GoFit/Controllers/HomeController.cs
@@ -18,13 +18,59 @@ namespace GoFit.Controllers
         /// Returns a list of workouts from the DB
         /// GET: /home/
         /// </summary>
+        /// <param name="sortBy">String parameter telling the controller how to sore the workouts</param>
         /// <returns>A list of workouts from the DB</returns>
         [AllowAnonymous]
-        public ActionResult Index()
+        public ActionResult Index(string sortBy)
         {
-            var workouts = from w in db.workouts select w;
+            var workouts = this.sortResults(sortBy);
             var view = View("Index", workouts.ToList());
             return view;
+        }
+
+        /// <summary>
+        /// Private helper method set and return the sorted workouts
+        /// </summary>
+        /// <param name="sortBy">Indicates the sort order</param>
+        /// <returns>The sorted workouts</returns>
+        private IQueryable<workout> sortResults(string sortBy)
+        {
+            var workouts = from w in db.workouts select w;
+
+            ViewBag.NameSortParam = (String.IsNullOrEmpty(sortBy)) ? "name_desc" : "";
+            ViewBag.DateSortParam = (sortBy == "date") ? "date_desc" : "date";
+            ViewBag.CategorySortParam = (sortBy == "category") ? "category_desc" : "category";
+            ViewBag.UserSortParam = (sortBy == "user") ? "user_desc" : "user";
+
+            switch (sortBy)
+            {
+                case "name_desc":
+                    workouts = workouts.OrderByDescending(w => w.name);
+                    break;
+                case "date":
+                    workouts = workouts.OrderBy(w => w.created_at);
+                    break;
+                case "date_desc":
+                    workouts = workouts.OrderByDescending(w => w.created_at);
+                    break;
+                case "category":
+                    workouts = workouts.OrderBy(w => w.category.name);
+                    break;
+                case "category_desc":
+                    workouts = workouts.OrderByDescending(w => w.category.name);
+                    break;
+                case "user":
+                    workouts = workouts.OrderBy(w => w.user.username);
+                    break;
+                case "user_desc":
+                    workouts = workouts.OrderByDescending(w => w.user.username);
+                    break;
+                default:
+                    workouts = workouts.OrderBy(w => w.name);
+                    break;
+            }
+
+            return workouts;
         }
     }
 }
