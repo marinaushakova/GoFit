b69ed8872a36e1ec42578d1e07ef5a4efc1529b8
Marina Ushakova <mushako1@my.westga.edu>
2015-06-15 23:00:01 -0700

    Created My Workouts index page showing list of all workouts of currently logged in user.

--- GoFit/Controllers/MyWorkoutsController.cs
+++ GoFit/Controllers/MyWorkoutsController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GoFit.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,12 +9,19 @@ namespace GoFit.Controllers
 {
     public class MyWorkoutsController : Controller
     {
+
+        private masterEntities db = new masterEntities();
+        private int currUserId;
+
         //
         // GET: /MyWorkouts/
         [Authorize]
         public ActionResult Index()
         {
-            ActionResult view = View("MyWorkouts");
+            currUserId = db.users.Where(a => a.username.Equals(User.Identity.Name)).FirstOrDefault().id;
+            var user_workouts = from w in db.user_workout where w.user_id == currUserId select w;
+
+            ActionResult view = View("Index", user_workouts.ToList());
             return view;
         }
 	}
