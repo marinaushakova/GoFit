8d8b7914a1e5ef4fc50de37af6865de032daeebd
Marina Ushakova <mushako1@my.westga.edu>
2015-06-16 01:14:49 -0700

    User can filter My Workouts to view All, In Progress, Not Started and Completed workouts.

--- GoFit/Controllers/MyWorkoutsController.cs
+++ GoFit/Controllers/MyWorkoutsController.cs
@@ -16,10 +16,31 @@ namespace GoFit.Controllers
         //
         // GET: /MyWorkouts/
         [Authorize]
-        public ActionResult Index()
+        public ActionResult Index(string filterString)
         {
             currUserId = db.users.Where(a => a.username.Equals(User.Identity.Name)).FirstOrDefault().id;
             var user_workouts = from w in db.user_workout where w.user_id == currUserId select w;
+            
+            switch (filterString)
+            {
+                case "in_progress":
+                    user_workouts = from w in db.user_workout
+                                    where w.user_id == currUserId && w.date_started != null && w.date_finished == null
+                                    select w;
+                    break;
+                case "not_started":
+                    user_workouts = from w in db.user_workout
+                                    where w.user_id == currUserId && w.date_started == null && w.date_finished == null
+                                    select w;
+                    break;
+                case "completed":
+                    user_workouts = from w in db.user_workout
+                                    where w.user_id == currUserId && w.date_started != null && w.date_finished != null
+                                    select w;
+                    break;
+                default:
+                    break;
+            }
 
             ActionResult view = View("Index", user_workouts.ToList());
             return view;
