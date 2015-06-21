c481509a39dc5d0264e5399e9cc3aa0acfb21bf4
Kevin Carlson <kcarlso2@my.westga.edu>
2015-06-21 16:15:00 -0400

    Added test file to MyWorkoutsController unit tests

---
+++ GoFit.Tests/Controllers/MyWorkoutsControllerTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GoFit.Controllers;
+
+namespace GoFit.Tests.Controllers
+{
+    [TestClass]
+    public class MyWorkoutsControllerTest
+    {
+        private MyWorkoutsController controller;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            //var mockContext = getDbContext();
+            //controller = new MyWorkoutsControllerTest(mockContext.Object);
+        }
+
+        [TestMethod]
+        public void TestAddWorkoutToMyWorkouts()
+        {
+
+        }
+    }
+}
