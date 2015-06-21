8326ca7404f38568f88bc813f6256ce147e3c605
Marina Ushakova <mushako1@my.westga.edu>
2015-06-20 20:35:28 -0700

    Password is encrypted before it's stored to db when user edits MyProfile

--- GoFit/Controllers/MyProfileController.cs
+++ GoFit/Controllers/MyProfileController.cs
@@ -3,6 +3,8 @@ using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Security;
@@ -34,13 +36,16 @@ namespace GoFit.Controllers
         public ActionResult Edit(user user)
         {
             masterEntities dbEntities = new masterEntities();
+            string hashedPassword = HashPassword(user.username, user.password);
+            user.password = hashedPassword;
+
             if (ModelState.IsValid)
             {
                 user.timestamp = DateTime.Now;
                 dbEntities.Entry(user).State = EntityState.Modified;
 
                 dbEntities.SaveChanges();
-                
+
                 return RedirectToAction("Index");
 
             }
@@ -48,5 +53,34 @@ namespace GoFit.Controllers
             return View(dbEntities.users.Where(a => a.username.Equals(User.Identity.Name)).FirstOrDefault());
 
         }
-	}
+
+        /// <summary>
+        /// Hashes the given data
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        private string HashData(string data)
+        {
+            SHA256 hasher = SHA256Managed.Create();
+            byte[] hashedData = hasher.ComputeHash(Encoding.Unicode.GetBytes(data));
+
+            StringBuilder sb = new StringBuilder(hashedData.Length * 2);
+            foreach (byte b in hashedData)
+            {
+                sb.AppendFormat("{0:x2}", b);
+            }
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Hashes the user login credentials
+        /// </summary>
+        /// <param name="userName"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        private string HashPassword(string userName, string password)
+        {
+            return HashData(String.Format("{0}{1}", userName.Substring(0, 4), password));
+        }
+    }
 }
