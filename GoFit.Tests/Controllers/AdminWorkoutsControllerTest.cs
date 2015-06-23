b581a1f50aaa628e778818c2894218ddf477d3d7
David Juliano <djulian1@my.westga.edu>
2015-06-23 15:08:37 -0400

    Add test class for AdminWorkoutsController

---
+++ GoFit.Tests/Controllers/AdminWorkoutsControllerTest.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GoFit;
+using GoFit.Controllers;
+using GoFit.Models;
+using PagedList;
+using Moq;
+using System.Data.Entity;
+using GoFit.Tests.MockContexts;
+using GoFit.Tests.MockSetupHelpers;
+
+namespace GoFit.Tests.Controllers
+{
+    [TestClass]
+    public class AdminWorkoutsControllerTest
+    {
+        private AdminWorkoutsController adminCon;
+        private Mock<masterEntities> db;
+        private WorkoutSearch search;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            DbContextHelpers contextHelpers = new DbContextHelpers();
+            search = new WorkoutSearch();
+
+            db = contextHelpers.getDbContext();
+            adminCon = new AdminWorkoutsController()
+            {
+                ControllerContext = MockContext.AuthenticationContext("admin")
+            };
+        }
+
+        [TestMethod]
+        public void TestSortAdminWorkoutsAsc()
+        {
+            string sortBy = "name";
+            // controller.action(args) as ViewResult
+            //  -gives a resulting view object
+            ViewResult result = adminCon.Index(null, sortBy, null, search) as ViewResult;
+            Assert.IsNotNull(result);
+            var workouts = (PagedList<workout>)result.ViewData.Model;
+            var isSortedAsc = this.isSorted(workouts, "name", "asc");
+            Assert.IsTrue(isSortedAsc);
+            
+        }
+
+        /* Private Test Helpers */
+
+        /// <summary>
+        /// Helper method to determin if a workout list is sorted in a certain order on a certain property
+        /// </summary>
+        /// <param name="workouts">The workout list to check</param>
+        /// <param name="propName">The workout property that the list should be sorted by</param>
+        /// <param name="order">One of "asc" or "desc". Tells the method to check if the list is in ascending or descending order</param>
+        /// <returns>True if the list is sorted on the given property in the given order, false otherwise</returns>
+        private bool isSorted(PagedList<workout> workouts, string propName, string order)
+        {
+            int limit = (workouts.Count > 10) ? 11 : workouts.Count;
+            for (int i = 1; i < limit; i++)
+            {
+                workout currentWorkout = workouts[i];
+                workout prevWorkout = workouts[i - 1];
+                int? res = null;
+                if (propName == "name")
+                {
+                    res = String.Compare(prevWorkout.name, currentWorkout.name);
+                }
+                else if (propName == "category")
+                {
+                    res = String.Compare(prevWorkout.category.name, currentWorkout.category.name);
+                }
+                else if (propName == "dateCreated")
+                {
+                    res = DateTime.Compare(prevWorkout.created_at, currentWorkout.created_at);
+                }
+                else if (propName == "username")
+                {
+                    res = String.Compare(prevWorkout.user.username, currentWorkout.user.username);
+                }
+                else return false;
+
+                if (order == "asc")
+                {
+                    if (res > 0) return false;
+                }
+                else if (order == "desc")
+                {
+                    if (res < 0) return false;
+                }
+            }
+            return true;
+        }
+    }
+}
