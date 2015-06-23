0ec156fb31db9464e3c71bea060571a90fc4b93f
David Juliano <djulian1@my.westga.edu>
2015-06-23 11:59:15 -0400

    Implement AdminExercise sort

---
+++ GoFit/Models/ExerciseSearch.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GoFit.Models
+{
+    public class ExerciseSearch
+    {
+        public ExerciseSearch()
+        {
+
+        }
+
+        public string name { get; set; }
+        public string description { get; set; }
+    }
+}
