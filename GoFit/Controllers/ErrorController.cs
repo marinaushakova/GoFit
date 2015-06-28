3b16da9e40d714d8d717a27891e9383d92374d3e
Kevin Carlson <kcarlso2@my.westga.edu>
2015-06-28 10:26:36 -0400

    Formatted DetailedError.html Changed NotFoundError() action's message

--- GoFit/Controllers/ErrorController.cs
+++ GoFit/Controllers/ErrorController.cs
@@ -7,18 +7,29 @@ using System.Web.Mvc;
 
 namespace GoFit.Controllers
 {
+    /// <summary>
+    /// Handles various application errors displaying the appropriate error message
+    /// </summary>
     public class ErrorController : Controller
     {
-        //
+        
         // GET: /Error/
+        /// <summary>
+        /// Default non descript error
+        /// </summary>
+        /// <returns>The plain error view</returns>
         public ActionResult Index()
         {
-            return View();
+            return View("Error");
         }
 
+        /// <summary>
+        /// Displays a not found error
+        /// </summary>
+        /// <returns>Not found view</returns>
         public ActionResult NotFoundError()
         {
-            return View("DetailedError", new HttpStatusCodeResult(HttpStatusCode.NotFound, "The requested resource could not be found")); 
+            return View("DetailedError", new HttpStatusCodeResult(HttpStatusCode.NotFound, "The requested resource does not exist or could not be found")); 
         }
 	}
 }
