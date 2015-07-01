9801d719489cbeeebadc404fd03f5c2cedc7ca34
Marina Ushakova <mushako1@my.westga.edu>
2015-07-01 02:30:13 -0700

    Added message validation in comment model - field is required

--- GoFit/Controllers/HomeController.cs
+++ GoFit/Controllers/HomeController.cs
@@ -290,7 +290,7 @@ namespace GoFit.Controllers
                 {
                     db.comments.Add(comment);
                     db.SaveChanges();
-                    return PartialView();
+                    return PartialView("AddComment");
                 }
                 catch
                 {
