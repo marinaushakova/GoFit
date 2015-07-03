4bcb4fdb25fb62e9aeca8c5019611bc15c91c143
David Juliano <djulian1@my.westga.edu>
2015-07-03 10:11:09 -0400

    Add time-based greeting to AdminHome Index. Remove user-style link in shared layout and replace with administrator link.

--- GoFit/Controllers/AdminHomeController.cs
+++ GoFit/Controllers/AdminHomeController.cs
@@ -20,6 +20,16 @@ namespace GoFit.Controllers
         // GET: AdminHome
         public ActionResult Index()
         {
+            int hour = DateTime.Now.Hour;
+            string time = "Evening";
+            if (hour < 12)
+                time = "Morning";
+            else if (hour >= 12 && hour < 18)
+                time = "Afternoon";
+            else
+                time = "Evening";
+
+            ViewBag.greeting = "Good " + time + ", " + User.Identity.Name + "!";
             return View();
         }
     }
