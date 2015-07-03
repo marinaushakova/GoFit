de4dcef5bc1f4ff69c906e685c4cec6dc0c05fdc
Marina Ushakova <mushako1@my.westga.edu>
2015-07-03 08:44:48 -0700

    Added Add to Favorites button to workout datails view. Implemented AddWorkoutToFavorites method in FavoriteWorkouts controller.

---
+++ GoFit/Controllers/FavoriteWorkoutsController.cs
@@ -0,0 +1,89 @@
+﻿using GoFit.Controllers.ControllerHelpers;
+using GoFit.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity;
+using System.Net;
+using System.Net.Http;
+
+namespace GoFit.Controllers
+{
+    public class FavoriteWorkoutsController : GoFitBaseController
+    {
+        private masterEntities db;
+        private int currUserId;
+        private UserAccess userAccess;
+
+        /// <summary>
+        /// Getter/setter for the pageSize instance variable
+        /// </summary>
+        public int pageSize { get; set; }
+
+        /// <summary>
+        /// Constructor to create the default db context
+        /// </summary>
+        public FavoriteWorkoutsController() : base ()
+        {
+            db = this.getDB();
+            userAccess = new UserAccess(db);
+        }
+
+        /// <summary>
+        /// Constructor to allow a passed in db context
+        /// </summary>
+        /// <param name="context">The context to use</param>
+        public FavoriteWorkoutsController(masterEntities context) : base(context)
+        {
+            db = this.getDB();
+            userAccess = new UserAccess(db);
+        }
+        //
+        // GET: /FavoriteWorkouts/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult AddWorkoutToFavorites(int? workout_id)
+        {
+            if (workout_id == null)
+            {
+                return View("DetailedError", new HttpStatusCodeResult(HttpStatusCode.BadRequest, "No workout id was specified."));
+            }
+            
+            int userID = userAccess.getUserId(User.Identity.Name);
+            if (userID == -1)
+            {
+                return View();
+            }
+
+            user_favorite_workout fav_workout = new user_favorite_workout();
+            fav_workout.user_id = userID;
+            fav_workout.workout_id = (int)workout_id;
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    db.user_favorite_workout.Add(fav_workout);
+                    db.SaveChanges();
+                    return RedirectToAction("Details", "Home", new { workoutId = workout_id });
+                }
+                catch (Exception ex)
+                {
+                    return View("DetailedError", new HttpStatusCodeResult(HttpStatusCode.InternalServerError, "Failed to add the requested workout to favorites."));
+                }
+            }
+            else
+            {
+                return View("DetailedError", new HttpStatusCodeResult(HttpStatusCode.BadRequest, "Failed to add the requested workout to favorites."));
+            }
+
+        }
+	}
+}
