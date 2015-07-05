8ecec9f29b7713453a694ae755bc305d2ce13a33
David Juliano <djulian1@my.westga.edu>
2015-07-05 12:01:02 -0400

    Fix AdminHome render index test. Ensure it is passing.

--- GoFit.Tests/Controllers/AdminHomeControllerTest.cs
+++ GoFit.Tests/Controllers/AdminHomeControllerTest.cs
@@ -3,6 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using GoFit.Controllers;
 using GoFit.Models;
 using PagedList;
+using GoFit.Tests.MockContexts;
 
 namespace GoFit.Tests.Controllers
 {
@@ -12,15 +13,25 @@ namespace GoFit.Tests.Controllers
 
         private AdminHomeController adminCon;
 
-        ///// <summary>
-        ///// Test that the AdminTypes Index view returns data
-        ///// </summary>
-        //[TestMethod]
-        //public void TestAdminHomeIndexViewRender()
-        //{
-        //    this.adminCon = new AdminHomeController();
-        //    ViewResult result = adminCon.Index() as ViewResult;
-        //    Assert.IsNotNull(result);
-        //}
+        [TestInitialize]
+        public void Initialize()
+        {
+            this.adminCon = new AdminHomeController()
+            {
+                // sign in as admin
+                ControllerContext = MockContext.AuthenticationContext("admin")
+            };
+            
+        }
+
+        /// <summary>
+        /// Test that the AdminTypes Index view returns data
+        /// </summary>
+        [TestMethod]
+        public void TestAdminHomeIndexViewRender()
+        {
+            ViewResult result = adminCon.Index() as ViewResult;
+            Assert.IsNotNull(result);
+        }
     }
 }
