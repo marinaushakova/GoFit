037687c3282c93a2d5d7f1e47aa8a17e7f4cdbf5
Kevin Carlson <kcarlso2@my.westga.edu>
2015-07-06 20:46:57 -0400

    Fixed null pointer exception on user edits username by resetting the Authorization Cookie

--- GoFit/Controllers/MyProfileController.cs
+++ GoFit/Controllers/MyProfileController.cs
@@ -86,6 +86,7 @@ namespace GoFit.Controllers
                         entry.CurrentValues.SetValues(user);
                     }
                     db.SaveChanges();
+                    FormsAuthentication.SetAuthCookie(user.username, false);
                     return RedirectToAction("Index", "MyProfile");
                 }
                 catch (DbUpdateConcurrencyException ex)
