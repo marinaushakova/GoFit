518591d9b3888aa9b95da07d70901348f524b723
Kevin Carlson <kcarlso2@my.westga.edu>
2015-07-06 20:56:54 -0400

    Updated test for AuthSession reset on edit user

--- GoFit/Controllers/MyProfileController.cs
+++ GoFit/Controllers/MyProfileController.cs
@@ -86,7 +86,7 @@ namespace GoFit.Controllers
                         entry.CurrentValues.SetValues(user);
                     }
                     db.SaveChanges();
-                    FormsAuthentication.SetAuthCookie(user.username, false);
+                    if (Session.Contents != null && Session.SessionID != null) FormsAuthentication.SetAuthCookie(user.username, false);
                     return RedirectToAction("Index", "MyProfile");
                 }
                 catch (DbUpdateConcurrencyException ex)
