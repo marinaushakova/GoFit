d004220e701ff123a3752a270e1c188b1848e94f
Kevin Carlson <kcarlso2@my.westga.edu>
2015-07-10 11:13:09 -0400

    Issue #64. Added empty static class recommender to do calculations for a user's recommended workout

---
+++ GoFit/Controllers/ControllerHelpers/Recommender.cs
@@ -0,0 +1,17 @@
+﻿using GoFit.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GoFit.Controllers.ControllerHelpers
+{
+    /// <summary>
+    /// Calculates recommended workouts based on a given user's completed workout 
+    /// history
+    /// </summary>
+    public static class Recommender
+    {
+        
+    }
+}
