4be2c20b572517db0c8d4648e15f2325935a64e9
Kevin Carlson <kcarlso2@my.westga.edu>
2015-07-11 12:26:55 -0400

    Issue #64. Added test file with initializer for Recommender class

---
+++ GoFit.Tests/Modules/RecommenderTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GoFit.Models;
+using Moq;
+using GoFit.Tests.MockSetupHelpers;
+using GoFit.Controllers.ControllerHelpers;
+
+namespace GoFit.Tests.Modules
+{
+    [TestClass]
+    public class RecommenderTest
+    {
+        private Mock<masterEntities> db;
+        private Recommender recommender;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            DbContextHelpers contextHelpers = new DbContextHelpers();
+            db = contextHelpers.getDbContext();
+            recommender = new Recommender(db.Object);
+        }
+
+        [TestMethod]
+        public void TestRecommendReturnsCorrectWorkouts()
+        {
+
+        }
+    }
+}
