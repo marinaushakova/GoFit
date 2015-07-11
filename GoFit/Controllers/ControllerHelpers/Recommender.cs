73e147f16db7ed490f068d11c43b3b1c2f327442
Kevin Carlson <kcarlso2@my.westga.edu>
2015-07-11 11:21:06 -0400

    Issue #64. Rafactored recommend method setup into helper methods as appropriate

--- GoFit/Controllers/ControllerHelpers/Recommender.cs
+++ GoFit/Controllers/ControllerHelpers/Recommender.cs
@@ -32,35 +32,20 @@ namespace GoFit.Controllers.ControllerHelpers
         public workout Recommend(int userID)
         {
             // Get a list of all categories
-            List<category> categoryList = db.Set<category>().ToList();
+            List<category> categoryList = getFullCategoryList();
             if (categoryList.Count < 1) return null;
 
             // get the users completed workouts
-            var completed = db.user_workout.Where(
-                w => w.user_id == userID && 
-                w.date_finished != null
-            );
+            var completed = getCompletedWorkoutsForUser(userID);
             if (completed.Count() < 1) return null;
             List<user_workout> completedList = completed.ToList();
             List<int> completedIdList = completed.Select(c => c.workout_id).ToList();
 
             // Set up a disctionary with a key for each category to track the number of completed exercises in each category
-            Dictionary<string, int> categoryCount = new Dictionary<string, int>();
-            foreach (category current in categoryList)
-            {
-                categoryCount.Add(current.name, 0);
-            }
-
-            // Tally the completed workouts for each category
-            foreach (user_workout current in completedList)
-            {
-                string currentCategory = current.workout.category.name;
-                categoryCount[currentCategory] += 1;
-            }
+            Dictionary<string, int> categoryCount = getCategoryTally(categoryList, completedList);
 
             // Generate a random number to determin the algorithm to use
-            Random random = new Random();
-            int algKey = random.Next(1, 3);
+            int algKey = getRandomNum(1, 2);
             algKey = 1;
 
             workout recommendation = null;
@@ -97,6 +82,69 @@ namespace GoFit.Controllers.ControllerHelpers
             }
 
             return recommendation;
+        }
+
+        /// <summary>
+        /// Gets a random number between the given bounds inclusive on both ends
+        /// </summary>
+        /// <param name="min">The min boundary (inclusive)</param>
+        /// <param name="max">The max boundary (inclusive)</param>
+        /// <returns>The random int withing the inclusive boundaries</returns>
+        private int getRandomNum(int min, int max)
+        {
+            max += 1;
+            Random random = new Random();
+            int num = random.Next(min, max);
+            return num;
+        }
+
+        /// <summary>
+        /// Gets a list of all the categories as a list
+        /// </summary>
+        /// <returns>The list of all the categories</returns>
+        private List<category> getFullCategoryList()
+        {
+            List<category> categoryList = db.Set<category>().ToList();
+            return categoryList;
+        }
+
+        /// <summary>
+        /// Gets an IQueryable list of the workouts completed by the given user id
+        /// </summary>
+        /// <param name="userID">The user ID to get completed workouts for</param>
+        /// <returns>The queryable list of completed workouts for the user</returns>
+        private IQueryable<user_workout> getCompletedWorkoutsForUser(int userID)
+        {
+            var completed = db.user_workout.Where(
+                w => w.user_id == userID &&
+                w.date_finished != null
+            );
+            return completed;
+        }
+
+        /// <summary>
+        /// Sets up a dictionary with keys as category names and values as the number of 
+        /// workouts a given user has completed in that category
+        /// </summary>
+        /// <param name="categoryList">List of all categories</param>
+        /// <param name="completedWorkoutList">List of all completed workouts for a given user</param>
+        /// <returns>The Dictionary showing the total number of workouts completed in each category by the given user</returns>
+        private Dictionary<string, int> getCategoryTally(List<category> categoryList, List<user_workout> completedWorkoutList)
+        {
+            // Set up a disctionary with a key for each category to track the number of completed exercises in each category
+            Dictionary<string, int> categoryCount = new Dictionary<string, int>();
+            foreach (category current in categoryList)
+            {
+                categoryCount.Add(current.name, 0);
+            }
+
+            // Tally the completed workouts for each category
+            foreach (user_workout current in completedWorkoutList)
+            {
+                string currentCategory = current.workout.category.name;
+                categoryCount[currentCategory] += 1;
+            }
+            return categoryCount;
         }
     }
 }
