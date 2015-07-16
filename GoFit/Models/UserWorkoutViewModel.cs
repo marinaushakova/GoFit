36f9e7efb33db2be546e9bc362fd6b708d0a797b
David Juliano <djulian1@my.westga.edu>
2015-07-16 19:38:22 -0400

    #66 Add a ViewModel in order to pass multiple models' information to the MyProfile and _BragFeedPartial views

---
+++ GoFit/Models/UserWorkoutViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GoFit.Models
+{
+    public class UserWorkoutViewModel
+    {
+        public GoFit.Models.user User { get; set; }
+        public List<GoFit.Models.user_workout> UserWorkoutList { get; set; }
+    }
+}
