740b3c82edb764e92fe064d2ce3c43927ae38b05
David Juliano <djulian1@my.westga.edu>
2015-07-16 19:47:46 -0400

    #66 Begin implementing code to pass the ViewModel data (brag feed list and user info) to the profile view

--- GoFit/Models/UserWorkoutViewModel.cs
+++ GoFit/Models/UserWorkoutViewModel.cs
@@ -7,7 +7,7 @@ namespace GoFit.Models
 {
     public class UserWorkoutViewModel
     {
-        public GoFit.Models.user User { get; set; }
-        public List<GoFit.Models.user_workout> UserWorkoutList { get; set; }
+        public user User { get; set; }
+        public List<user_workout> UserWorkoutList { get; set; }
     }
 }
